d3e8bd60bc803626cbbf5272df8364300c6ac464
Alexlr10 <alexlr10@hotmail.com>
2020-11-09 21:04:12 -0300

    inserindo metodo post e get e dockerizando projeto

--- legacy/settings.py
+++ legacy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1he)wrz+7q7lzc98n2x1rnz=vt_nfxe9cv!0p_v*jb8(b8x2kb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
