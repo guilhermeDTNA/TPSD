ec299505d915b77d8de686ccde2e1a5ad6727abd
Alexlr10 <alexlr10@hotmail.com>
2020-11-24 21:08:44 -0300

    atualizando comandos npm no dockerfile do frontend

--- front_end/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose run react npm install react-router-dom
-docker-compose run react npm install react-icons
-docker-compose run react npm install axios
-docker-compose run react npm install react-bootstrap
